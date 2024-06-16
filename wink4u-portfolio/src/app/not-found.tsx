cb989f3028c08d00f37327ca4d920799732fd406
김승현 <kim0221_@naver.com>
2024-06-17 02:35:37 +0900

    update: not-found 404 page

---
+++ wink4u-portfolio/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const NotFound: NextPage = () => {
+    return(
+        <div>
+          <div>이 페이지는 존재하지 않습니다.</div>
+        </div>
+    )
+}
+
+export default NotFound;

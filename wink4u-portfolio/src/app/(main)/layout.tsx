51c276ad7feb0e5ceb437a64afb27dad6a855b97
김승현 <kim0221_@naver.com>
2024-06-17 02:36:04 +0900

    update: 기본 layout 설정

---
+++ wink4u-portfolio/src/app/(main)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+
+type Props = { children : ReactNode};
+
+export default function Layout({ children } : Props) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}

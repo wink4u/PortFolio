be2dc85adc172ece5a7c384bfe21d2fa7e6c52de
김승현 <kim0221_@naver.com>
2024-06-19 03:22:55 +0900

    feat: 초기 infoContent 구성

---
+++ wink4u-portfolio/src/app/(main)/_components/organisms/infoContent/infoContent.tsx
@@ -0,0 +1,11 @@
+import Title from "../../atoms/title/title";
+import InfoList from "../../molecules/infoList/infoList";
+
+export default function infoContent() {
+    return(
+        <div>
+          <Title value="Who I am" />
+          <InfoList />
+        </div>
+    )
+}

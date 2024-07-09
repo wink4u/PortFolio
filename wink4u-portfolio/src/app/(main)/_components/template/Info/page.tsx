8e30eb740007d44a725ee61758bc3f6153d9760c
김승현 <kim0221_@naver.com>
2024-07-09 22:34:12 +0900

    delete: 이전 component 삭제

--- wink4u-portfolio/src/app/(main)/_components/template/Info/page.tsx
+++
@@ -1,9 +0,0 @@
-import InfoContent from '../../organisms/infoContent/infoContent';
-
-export default function Info() {
-    return (
-        <div>
-          <InfoContent />
-        </div>
-    )
-}

c90c0cfecde2897cf4a255f2327cb209b31265a1
김승현 <kim0221_@naver.com>
2024-07-09 22:35:07 +0900

    feat: infoList Component

--- wink4u-portfolio/src/app/(main)/_components/molecules/infoList/infoList.tsx
+++ wink4u-portfolio/src/app/(main)/_components/molecules/infoList/infoList.tsx
@@ -1,13 +1,15 @@
 import INFO_LIST from "@/app/(main)/_constants/info";
 import InfoItem from "../../atoms/infoItem/infoItem";
-import styles from "./infolist.module.scss";
+import styles from "./infoList.module.scss";
 
 export default function InfoList() {
     return(
+      <div className={styles.infoListContainer}>
         <div className={styles.infoList}>  
           {INFO_LIST.map(item => (
               <InfoItem key={item.id} icon={item.icon} title={item.title} content={item.content} />
           ))}
         </div>
+      </div>
     )
 }

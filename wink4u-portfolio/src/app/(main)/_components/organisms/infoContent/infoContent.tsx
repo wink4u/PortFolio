efe6fa9e539108e03f5de666a9d38c2a53141347
김승현 <kim0221_@naver.com>
2024-07-18 03:51:33 +0900

    update: infoContent Introduce, ArchivingContent 추가

--- wink4u-portfolio/src/app/(main)/_components/organisms/infoContent/infoContent.tsx
+++ wink4u-portfolio/src/app/(main)/_components/organisms/infoContent/infoContent.tsx
@@ -1,11 +1,15 @@
+import Introduce from "../../atoms/introduce/introduce";
 import Title from "../../atoms/title/title";
 import InfoList from "../../molecules/infoList/infoList";
+import ArchivingContent from "../archivIngContent/archivingContent";
 
 export default function infoContent() {
     return(
         <div>
           <Title value="Who I am" />
+          <Introduce />
           <InfoList />
+          <ArchivingContent />
         </div>
     )
 }

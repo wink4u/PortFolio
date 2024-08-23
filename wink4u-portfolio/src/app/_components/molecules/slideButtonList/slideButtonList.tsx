864e2b5b3ba8e6206f48ee698ec86549be63aa33
김승현 <kim0221_@naver.com>
2024-08-24 04:37:49 +0900

    feat: Project Img ButtonSlider 적용 및 생성

---
+++ wink4u-portfolio/src/app/_components/molecules/slideButtonList/slideButtonList.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useState } from "react";
+import ImageSlider from "../../atoms/imageSlider/imageSlider";
+import SlideButtonItem from "../../atoms/slideButtonItem/slideButtonItem";
+import styles from './slideButtonList.module.scss';
+import { StaticImageData } from "next/image";
+
+interface SlideButtonListProps {
+  images: StaticImageData[];
+}
+
+export default function SlideButtonList({ images } : SlideButtonListProps) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handlePrevious = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    const handleSelectImage = (index: number) => {
+        setCurrentIndex(index);
+        console.log(index);
+    };
+
+    return (
+        <div className={styles.slideButtonListContainer}>
+          <ImageSlider images={images} currentIndex={currentIndex}/>
+          <div className={styles.slideButtonList}>
+            <SlideButtonItem buttonClick={handlePrevious} value="<" />
+            {images.map((_, index) => (
+                <SlideButtonItem key={index} buttonClick={() => handleSelectImage(index)} value={`${index + 1}`} />
+            ))}
+            <SlideButtonItem buttonClick={handleNext} value=">" />
+          </div>
+        </div>
+    );
+}

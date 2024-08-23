42f579b05aab85e67822f349cdb0f72674281c8f
김승현 <kim0221_@naver.com>
2024-08-24 04:34:59 +0900

    feat: InfoItem 컴퍼넌트 생성 (내 소개)

---
+++ wink4u-portfolio/src/app/_components/atoms/infoItem/infoItem.tsx
@@ -0,0 +1,22 @@
+import { IconType } from 'react-icons';
+import styles from './infoitem.module.scss';
+
+interface infoItemtProps {
+    icon: IconType;
+    title: string;
+    content: string;
+}
+
+export default function InfoItem({ icon: Icon, title, content}: infoItemtProps) {
+    return (
+      <div className={styles.infoItemContainer}>
+        <div className={styles.infoIconContent}>
+          <span className={styles.infoIcon}><Icon /></span>
+        </div>
+        <div className={styles.infoItemMain}>
+          <p className={styles.infoTitle}>{title}</p>
+          <p className={styles.infoContent}>{content}</p>
+        </div>
+      </div>
+    )
+}

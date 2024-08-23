7fcf5af6153d78c9380db546d2b310357bf1e69d
김승현 <kim0221_@naver.com>
2024-08-24 04:50:48 +0900

    feat: 기본 Layout

---
+++ wink4u-portfolio/src/app/_components/template/Layout/layout.tsx
@@ -0,0 +1,26 @@
+import LayoutBottomMargin, { LayoutTopMargin } from "@/app/_constants/margin";
+import LayoutPadding from "@/app/_constants/padding";
+import classNames from "classnames";
+import styles from './layout.module.scss';
+import { ReactNode } from "react";
+
+interface LayoutProps {
+    children: ReactNode;
+    marginTop?: LayoutTopMargin;
+    marginBottom?: LayoutBottomMargin;
+    padding?: LayoutPadding;
+}
+
+export default function Layout({ children, marginTop, marginBottom, padding, ...rest} : LayoutProps) {
+    return(
+        <div className={classNames(`${styles.layout}`, marginTop, marginBottom, padding)} {...rest}>
+            {children}
+        </div>
+    )
+}
+
+Layout.defaultProps = {
+    marginTop: LayoutTopMargin.mt0,
+    marginBottom: LayoutBottomMargin.mb0,
+    padding: LayoutPadding.p0,
+}

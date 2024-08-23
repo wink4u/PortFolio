3f16e0d68260c86b20b3f2498a147b5a037ae0d0
김승현 <kim0221_@naver.com>
2024-08-24 04:50:21 +0900

    feat: ContentsLayout

---
+++ wink4u-portfolio/src/app/_components/template/ContentsLayout/contentsLayout.tsx
@@ -0,0 +1,30 @@
+import LayoutBottomMargin, { LayoutLeftRightMargin, LayoutTopMargin } from "@/app/_constants/margin";
+import LayoutPadding from "@/app/_constants/padding";
+import classNames from "classnames";
+import { ReactNode } from "react";
+
+interface ContentsLayoutProps {
+    children: ReactNode;
+    marginTop?: LayoutTopMargin;
+    marginBottom?: LayoutBottomMargin;
+    marginLR?: LayoutLeftRightMargin;
+    padding?: LayoutPadding;
+
+}
+
+function ContentsLayout({ children, marginTop, marginBottom, marginLR, padding, ...rest } : ContentsLayoutProps) {
+    return (
+        <div className={classNames('contentsLayoutContainer', marginTop, marginBottom, marginLR, padding)} {...rest}>
+          {children}
+        </div>
+    )
+}
+
+export default ContentsLayout;
+
+ContentsLayout.defaultProps = {
+    marginTop: LayoutTopMargin.mt0,
+    marginBottom: LayoutBottomMargin.mb30,
+    marginLR: LayoutLeftRightMargin.m0,
+    padding: LayoutPadding.p0,
+}

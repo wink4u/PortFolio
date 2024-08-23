9532d51fd637bafbe6a697048aa72aa0ceb10aef
김승현 <kim0221_@naver.com>
2024-08-24 04:52:41 +0900

    feat: Skill page 적용

---
+++ wink4u-portfolio/src/app/skill/page.tsx
@@ -0,0 +1,10 @@
+import Skill from '../_components/page/Skill/page'
+import styles from './skill.module.scss'
+
+export default function SkillPage() {
+    return(
+      <div className={styles.skillContainer}>
+        <Skill />
+      </div>
+    )
+}

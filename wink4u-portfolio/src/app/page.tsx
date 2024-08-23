2ae3c642b23b88895434453bf73a350e46f87eab
김승현 <kim0221_@naver.com>
2024-08-24 04:32:45 +0900

    feat: Info Main페이지 적용

---
+++ wink4u-portfolio/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Info from "./_components/page/Info/page";
+import styles from "./page.module.scss"
+export default function Main() {
+    return (
+      <div className={styles.infoContainer}>
+        <Info />
+      </div>
+    )
+}

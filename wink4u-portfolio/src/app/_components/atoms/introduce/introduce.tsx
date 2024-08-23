6646f2e7d56814558a41a9b211c4627f2b5a86c6
김승현 <kim0221_@naver.com>
2024-08-24 04:35:17 +0900

    feat: Info Introduce 컴퍼넌트 생성

---
+++ wink4u-portfolio/src/app/_components/atoms/introduce/introduce.tsx
@@ -0,0 +1,11 @@
+import styles from './introduce.module.scss';
+
+export default function Introduce() {
+    return(
+        <div className={styles.IntroduceContainer}>
+          <p>안녕하세요</p>
+          <p>학습을 좋아하는 프론트엔드 개발자, 김승현입니다.</p>
+        </div>
+    )
+
+} 

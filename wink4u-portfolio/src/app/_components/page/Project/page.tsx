73c5b1aba15a37a84f1fe1bf5fb846112b2122d9
김승현 <kim0221_@naver.com>
2024-08-26 21:33:00 +0900

    fix: 폴더구조 변경으로 인한 import 처리

--- wink4u-portfolio/src/app/_components/page/Project/page.tsx
+++ wink4u-portfolio/src/app/_components/page/Project/page.tsx
@@ -1,6 +1,6 @@
 import Title from "../../atoms/title/title";
 import ProjectTemplate from "../../template/ProjectTemplate/projectTemplate";
-import ProjectList from "../../organisms/projectList/projectList";
+import ProjectList from "../../molecules/projectList/projectList";
 
 export default function Project() {
     return (

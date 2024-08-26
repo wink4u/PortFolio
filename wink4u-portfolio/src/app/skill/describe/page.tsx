a67b7c5ba02baafc602d603ab44fa3fc5de1b2da
김승현 <kim0221_@naver.com>
2024-08-26 21:33:33 +0900

    test: intercepting Route page 테스트

---
+++ wink4u-portfolio/src/app/skill/describe/page.tsx
@@ -0,0 +1,9 @@
+import SkillDescribe from "@/app/_components/organisms/skillDescribe/skillDescribe"
+
+export default function SDescribe(){
+  return(
+    <>
+      <SkillDescribe />
+    </>
+  )
+}

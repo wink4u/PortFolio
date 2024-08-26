1f76983d58ec75b44e0f9e822b7ecbfad651cd33
김승현 <kim0221_@naver.com>
2024-08-26 21:35:52 +0900

    feat: interCepting Route SDescribe

---
+++ wink4u-portfolio/src/app/@modal/(.)skill/describe/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SkillDescribe from "../../../_components/organisms/skillDescribe/skillDescribe";
+
+export default function SDescribe(){
+  return(
+    <>
+      <SkillDescribe />
+    </>
+  )
+}

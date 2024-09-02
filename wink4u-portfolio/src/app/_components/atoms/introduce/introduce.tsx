61ae4ff1e41c9288818dabe2643c73d11348d0ad
김승현 <kim0221_@naver.com>
2024-09-02 20:46:29 +0900

    modify: Introduce 머리말 수정

--- wink4u-portfolio/src/app/_components/atoms/introduce/introduce.tsx
+++ wink4u-portfolio/src/app/_components/atoms/introduce/introduce.tsx
@@ -4,7 +4,7 @@ export default function Introduce() {
     return(
         <div className={styles.IntroduceContainer}>
           <p>안녕하세요</p>
-          <p>학습을 좋아하는 프론트엔드 개발자, 김승현입니다.</p>
+          <p>성장하는 프론트엔드 개발자, 김승현입니다.</p>
         </div>
     )
 

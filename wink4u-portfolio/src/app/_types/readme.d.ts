70aeb71ee9618b0d1c495d5e0df28440810f1768
김승현 <kim0221_@naver.com>
2024-09-22 02:28:48 +0900

    feat: readme data type 설정

---
+++ wink4u-portfolio/src/app/_types/readme.d.ts
@@ -0,0 +1,53 @@
+import { StaticImageData } from "next/image";
+
+export interface readmeIntroData {
+    introImg: StaticImageData;
+    introContent: string[];
+}
+
+export interface reademeWhatIdoData {
+    id: number;
+    whatIdoImg: StaticImageData[];
+    whatIdoContent: WhatIdoContent[];
+}
+
+interface WhatIdoContent {
+    id: number;
+    title: string;
+    idoSub: WhatIdoSub[];
+}
+
+interface WhatIdoSub {
+    id: number;
+    subTitle: string;
+    ssubIdo?: WhatIdoSsub[];
+}
+
+interface WhatIdoSsub {
+    id: number;
+    ssubTitle: string;
+}
+
+interface TroubleItem {
+    id: number;
+    Img?: StaticImageData[];
+    content: string;
+}
+
+export interface readmeTroubleData {
+    id: number;
+    title: string;
+    troubleList: TroubleItem[];
+    solutionList: TroubleItem[];
+}
+
+
+export interface readmeItem {
+    readmeIntro: readmeIntroData;
+    reademeWhatIdo: reademeWhatIdoData[];
+    readmeTrouble: readmeTroubleData[];
+}
+
+export interface showReadme {
+    project: readmeItem;
+}
